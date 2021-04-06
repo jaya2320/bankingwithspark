6411b3e579fb58dfbac6a3989a9320fbf5c54f33
jaya2320 <jayagupta752@gmail.com>
2021-04-06 21:02:02 +0530

    first commit

--- system/urls.py
+++ system/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('customer',views.customer,name='customer'),
     path('transaction/<int:pk>',views.transaction,name='transaction'),
     path('transaction_history/<int:pk2>',views.transaction_history,name='transaction_history'),
+    path('make_transaction/index',views.index,name='index'),
     path('make_transaction/<int:pk1>',views.make_transaction,name='make_transaction'),
     path('delete_history/<int:pk3>',views.delete_history,name="delete_history"),
     path('delete_customer/<int:pk3>',views.delete_customer,name="delete_customer")
